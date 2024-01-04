a7468b246c908a92fb8f40ef677b63b180df4dfe
monpaoki <81399842+rikaisokai@users.noreply.github.com>
2024-01-04 09:31:26 -0800

    a helloworld cpp file

---
+++ helloworld.cpp
@@ -0,0 +1,8 @@
+// main.cpp
+#include <iostream> // Include the input/output library
+
+int main() // Define the main function
+{
+    std::cout << "Hello, World!" << std::endl; // Print "Hello, World!" to the standard output stream, followed by a newline
+    return 0; // Return 0 to indicate successful execution
+}
