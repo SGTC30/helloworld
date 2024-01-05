bc4118f85d5c3d523fb5b7cb134f2972c750b4bc
monpaoki <81399842+rikaisokai@users.noreply.github.com>
2024-01-04 23:16:47 -0800

    A variables.h file for hello world
    There are three variables using the extern keyword to be placed and used and modify helloworld.cpp

---
+++ variables.h.cpp
@@ -0,0 +1,13 @@
+
+// variables.h
+
+#ifndef VARIABLES_H
+#define VARIABLES_H
+
+// Declare and initialize variables
+extern int variable1;
+extern float variable2;
+extern char variable3;
+
+#endif
+
