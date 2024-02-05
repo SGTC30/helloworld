a63361ce3749413ea2872d9f7f34594021b226e4
Sagacity Tech LLC <81399842+SGTC30@users.noreply.github.com>
2024-02-05 08:02:56 -0800

    Update variables.h

--- variables.h
+++ variables.h
@@ -5,10 +5,10 @@
 #define VARIABLES_H
 
 // Declare and initialize variables
-extern int variable1;
-extern float variable2;
-extern const char* variable3;
-extern int variable4;
-extern float variable5;
-extern const char* variable6;
+extern int variable1=5;
+extern float variable2=4.68;
+extern const char* variable3="phione";
+extern int variable4=8;
+extern float variable5=7.68;
+extern const char* variable6="phitwo";
 #endif
