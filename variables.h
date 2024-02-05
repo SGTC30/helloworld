1ab0d8e2b7381c5496a3065b1e81cd2402b6507b
Sagacity Tech LLC <81399842+SGTC30@users.noreply.github.com>
2024-02-05 06:37:41 -0800

    Update

---
+++ variables.h
@@ -0,0 +1,14 @@
+
+// variables.h
+
+#ifndef VARIABLES_H
+#define VARIABLES_H
+
+// Declare and initialize variables
+extern int variable1;
+extern float variable2;
+extern char variable3;
+extern int variable4;
+extern float variable5;
+extern char variabl6;
+#endif
